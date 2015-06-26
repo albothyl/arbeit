c176202da8ecb05f8441b487bfea4578a89b8067
albothyl <jjhwqqq@naver.com>
2015-06-26 21:23:13 +0900

    맴버, 유저 분리

--- src/main/java/com/job/common/springSecurity/service/DefaultAuthenticationService.java
+++ src/main/java/com/job/common/springSecurity/service/DefaultAuthenticationService.java
@@ -3,6 +3,7 @@ package com.job.common.springSecurity.service;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.dao.ReflectionSaltSource;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.util.StringUtils;
 import com.job.common.springSecurity.domain.UserDetail;
 import com.job.common.springSecurity.domain.UserDetailRepository;
 
+@Slf4j
 @Service
 public class DefaultAuthenticationService implements AuthenticationService {
 
@@ -23,6 +25,7 @@ public class DefaultAuthenticationService implements AuthenticationService {
 		UserDetail userDetail = userDetailRepository.getByEmail(email);
 		userDetail.getAuthorities().size();
 		userDetail.getPassword();
+
 		return userDetail;
 	}
 
@@ -30,7 +33,7 @@ public class DefaultAuthenticationService implements AuthenticationService {
 	public void saveUser(@NotNull(message = "{validate.authenticate.saveUser}") @Valid UserDetail userDetail, String newPassword) {
 		if (!StringUtils.isEmpty(newPassword)) {
 			ShaPasswordEncoder shaPasswordEncoder = new ShaPasswordEncoder(256);
-			shaPasswordEncoder.encodePassword(newPassword, new ReflectionSaltSource());
+			userDetail.setPassword(shaPasswordEncoder.encodePassword(newPassword, new ReflectionSaltSource()));
 		}
 		userDetailRepository.save(userDetail);
 	}
