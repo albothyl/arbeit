5cbe98f70c3068d916fce1af63c86dcf7cde10e7
albothyl <jjhwqqq@naver.com>
2015-07-05 00:07:14 +0900

    리엑트 튜토리얼

---
+++ src/main/webapp/react/tutorial6.js
@@ -0,0 +1,117 @@
+//서버에 데이터를 저장하고, 화면에 보여준다.
+
+var CommentForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var author = React.findDOMNode(this.refs.author).value.trim();
+		var text = React.findDOMNode(this.refs.text).value.trim();
+		if (!text || !author) {
+			return;
+		}
+
+		// 서버에 요청을 전송합니다
+		this.props.onCommentSubmit({author: author, text: text});
+
+		React.findDOMNode(this.refs.author).value = '';
+		React.findDOMNode(this.refs.text).value = '';
+		return;
+	},
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input type="text" placeholder="이름" ref="author" />
+				<input type="text" placeholder="내용을 입력하세요..." ref="text" />
+				<input type="submit" value="올리기" />
+			</form>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render: function() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function() {
+		if (this.props.data) {
+			var commentNodes = this.props.data.map(function (comment) {
+				return (
+					<Comment author={comment.author}>
+						{comment.text}
+					</Comment>
+				);
+			});
+		}
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		);
+	}
+});
+
+var CommentBox = React.createClass({
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleCommentSubmit: function(comment) {
+		//먼저 화면에 추가하고, 서버로 ajax 요청을 보낸다.
+		var comments = this.state.data;
+		var newComments = comments.concat([comment]);
+		this.setState({data: newComments});
+
+		$.ajax({
+			url: "/hello/saveData", //this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+	render: function() {
+		return (
+			<div className="commentBox">
+				<h1>댓글</h1>
+				<CommentList data={this.state.data} />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
+			</div>
+		);
+	}
+});
+
+React.render(
+	<CommentBox url="/hello/getData" pollInterval={2000} />,
+	document.getElementById('content')
+);
